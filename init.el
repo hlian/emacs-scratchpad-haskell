da165cea1ca6271b383866c137387ae9460080f5
Hao Lian <hao@trello.com>
2015-07-15 13:57:46 -0400

    yet more

--- init.el
+++ init.el
@@ -8,20 +8,40 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
+;; Stupid tilde files.
+(setq backup-directory-alist `(("." . "~/.saves")))
+
 ;; use-package! use-package is just a set of macros, so it only needs
 ;; to be loaded during compile time.
 (eval-when-compile
   (defvar use-package-verbose t)
   (require 'use-package))
 
-; You'll need this if you Haskell binaries aren't in the global OS X
-; $PATH (stored in the bowels of /etc)
+;; You'll need this if you Haskell binaries aren't in the global OS X
+;; $PATH (stored in the bowels of /etc)
 (exec-path-from-shell-initialize)
 
+;; This registers haskell-mode as an autoload for .hs files. It also
+;; registers a configuration block to run whenever haskell-mode is
+;; loaded. (So this should take 0 time on startup, deferring the work
+;; until the first .hs buffer is opened.)
 (use-package haskell-mode
    :mode "\\.hs\\'"
    :commands haskell-mode
    :config (progn
+	     ; Send code to GHCi.
+	     (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+	     ; Bring up GHCi.
+             (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+	     ; Restart GHCi.
+             (define-key haskell-mode-map (kbd "C-c C-r") 'haskell-process-restart)
+	     ; Insert type annotation.
+             (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+	     ; Build everything.
+             (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+	     ; Run arbitrary Cabal commands.
+             (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+
 	     ; Choose an indentation mode.
              (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 	     ; Choose an interaction mode.
