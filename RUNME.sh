9dacd0fec38aa89af6ed637a74f8f458cfaf4b7b
Hao Lian <hao@trello.com>
2015-09-16 13:37:14 -0400

    RUNME.sh: fix set -e bug h/t aarond

--- RUNME.sh
+++ RUNME.sh
@@ -5,16 +5,17 @@ set -eux
 US=$(dirname "$0")
 pushd $US > /dev/null
 
-CASK=$(which cask)
-if [[ $? == 0 ]]; then
-    echo "Found cask: $CASK"
-else
+CASK=$(which cask) || {
     echo "Installing cask..."
     curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
     which cask > /dev/null || (echo "Couldn't install cask!"; exit 1)
-fi
+}
 
+STACK=$(which stack) || {
+    echo "You need to install Stack from <https://github.com/commercialhaskell/stack/releases/tag/v0.1.4.0>."
+}
+
+echo "Found stack: $STACK"
+echo "Found cask: $CASK"
 cask install
-which runhaskell > /dev/null || (echo "Couldn't find runhaskell! You'll need it for Flycheck.")
-
 popd > /dev/null
